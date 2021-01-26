901866a21b56288fe48b122aa71a64bfcb6083de
darren.conroy <darren.conroy@tricron.com>
2021-01-26 18:36:02 +0000

    vSeeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -113,14 +113,9 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        //vSeeds.emplace_back("seed.shirecoin.sipa.be"); // Pieter Wuille, only supports x1, x5, x9, and xd
-        //vSeeds.emplace_back("dnsseed.bluematt.me"); // Matt Corallo, only supports x9
-        //vSeeds.emplace_back("dnsseed.shirecoin.dashjr.org"); // Luke Dashjr
-        //vSeeds.emplace_back("seed.shirecoinstats.com"); // Christian Decker, supports x1 - xf
-        //vSeeds.emplace_back("seed.shirecoin.jonasschnelli.ch"); // Jonas Schnelli, only supports x1, x5, x9, and xd
-        //vSeeds.emplace_back("seed.shire.petertodd.org"); // Peter Todd, only supports x1, x5, x9, and xd
-        //vSeeds.emplace_back("seed.shirecoin.sprovoost.nl"); // Sjors Provoost
-        //vSeeds.emplace_back("dnsseed.emzy.de"); // Stephan Oeste
+        vSeeds.emplace_back("51.195.203.209"); // UK
+        vSeeds.emplace_back("51.222.136.220"); // Canada
+        vSeeds.emplace_back("51.79.222.81"); // Singapore
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
